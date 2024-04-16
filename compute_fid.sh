c7d54714b2c8d271d52b1ef8d393750acba38432
yenchi <charlescheng0117@gmail.com>
2024-04-16 02:07:12 +0000

    udpate

--- compute_fid.sh
+++ compute_fid.sh
@@ -1,6 +1,13 @@
 
 gt_dir="/nfs/ycheng/proj/cfm-scheduler/CIFAR-10-images/train-all"
-gen_dir="logs/test_linear"
+# gen_dir="/nfs/ycheng/proj/cfm-scheduler/logs/test_linear/gen_images"
+# gen_dir="/nfs/ycheng/proj/cfm-scheduler/logs/test_exp/gen_images"
+gen_dir="/nfs/ycheng/proj/cfm-scheduler/logs/test_cos/gen_images"
+
+
+# linear: 24.8911
+# exp: 49.8486
+# cos: 27.9221
 
 gpu_id="4"
 
